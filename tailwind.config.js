b7c4077b976fc42a6fd5e38721a91a1a4de710e7
cyberchelle <grechelleboneo97@gmail.com>
2024-10-16 22:35:22 +0800

    fixed tailwind css modified:   package-lock.json modified:   package.json new file:   postcss.config.js modified:   src/index.js renamed:    src/tailwind.css -> src/styles/tailwind.css modified:   tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,9 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}", // Includes all JavaScript and TypeScript files inside the src folder
+    './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
